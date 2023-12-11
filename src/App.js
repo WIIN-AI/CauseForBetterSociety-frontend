cd09ea5a315d2e61b6b1e36ca957caab5884b274
sai24121994 <sai24121994@gmail.com>
2023-12-11 16:51:09 +0530

    added path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import Navbar from "./components/nav/nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./home/home";
-import NoPage from "./home/nopage";
+import Home from "./pages/home";
+import NoPage from "./pages/nopage";
 import { Container } from "@mui/material";
 import CreatePost from "./components/UI/createPost";
 import PostDetails from "./components/UI/post";
